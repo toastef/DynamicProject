1b69e1182153b44f4c181072f9bb3a751d59f34b
toastef <94532614+toastef@users.noreply.github.com>
2022-11-10 13:32:56 +0100

    user

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,14 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         return $this->render('login/login.html.twig', [
-            
+
         ]);
     }
 }
